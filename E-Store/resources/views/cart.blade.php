67a5c40b03ba8384bdd4354be088843886242bbb
yohocx <yohocx@gmail.com>
2022-05-04 02:10:25 +0500

    authorization and home page

---
+++ E-Store/resources/views/cart.blade.php
@@ -0,0 +1,6 @@
+@extends('master')
+@section('content')
+    <div class="cart">
+        <h1>Cart</h1>
+    </div>
+@endsection
---
+++ E-Store/resources/views/login.blade.php
@@ -0,0 +1,31 @@
+@extends('master')
+@section("content")
+
+    <main class="text-center form-signin login">
+        <form action="/login" method="POST">
+            <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
+            @csrf
+            <div class="form-floating">
+                <input type="email" class="form-control" name="email" id="floatingInput" placeholder="name@example.com">
+                <label for="floatingInput">Email address</label>
+            </div>
+            <div class="form-floating">
+                <input type="password" class="form-control" name="password" id="floatingPassword" placeholder="Password">
+                <label for="floatingPassword">Password</label>
+            </div>
+            <button class="btn btn-dark" type="submit">Sign in</button>
+        </form>
+    </main>
+
+    <style>
+        .login{
+            margin-top: 50px;
+            display: flex;
+            justify-content: center;
+        }
+        .form-floating{
+            margin-bottom: 10px;
+        }
+    </style>
+
+@endsection
